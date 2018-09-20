658d604d48a97062e16b7e7a8bd255f2b4364240
Karen Li <kli19@stuy.edu>
2018-09-19 22:41:52 -0400

    finished hw

--- 08_lemme_flask_u_sumpn/app.py
+++ 08_lemme_flask_u_sumpn/app.py
@@ -1,13 +1,27 @@
 from flask import Flask
 app = Flask(__name__) #create instance of class Flask
 
-@app.route("/") #assign function to route
-def hello_world():
-    print("about to print __name__...")
-    print(__name__)
-    return "No hablo queso"
+@app.route("/")
+def home():
+    return """
+    <h1> Japanese </h1>
+    <h3> <a href="/hiragana">hiragana</a> </h3>
+    <h3> <a href="/katakana">katakana</a> </h3>
+    """
+
+@app.route("/katakana")
+def katakana():
+    return """
+    <img src="https://files.tofugu.com/articles/japanese/2017-07-13-katakana-chart/wikipedia-katatkana-chart.jpg" alt="katakana chart">
+"""
+
+@app.route("/hiragana")
+def hiragana():
+    return """
+    <img src="https://files.tofugu.com/articles/japanese/2016-04-05-hiragana-chart/wikipedia-hiragana-chart.png" alt="hiragana chart"> 
+    """
 
 if __name__ == "__main__":
-    app.debug = True #turn off when going live
+    app.debug = False #turn off when going live
     app.run()
 
---
+++ 08_lemme_flask_u_sumpn/app.py~
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/") #assign function to route
+def hello_world():
+    print("about to print __name__...")
+    print(__name__)
+    return "No hablo queso"
+
+if __name__ == "__main__":
+    app.debug = True #turn off when going live
+    app.run()
+
