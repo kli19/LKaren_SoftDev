35b795ecbd72efea36d928efb4bd976fce517cde
Karen Li <kli19@stuy.edu>
2018-09-22 12:42:59 -0400

    restore old code

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -1,28 +1,71 @@
 from flask import Flask, render_template
-from divine import *
 
 app = Flask(__name__)
 
-csvFile = open('./occupations.csv') #read csv file
-diction = cvsDict(csvFile) #construct dictionary from the csv file
+import csv, random
 
-@app.route('/home') #home route
-def goToOccupations():
-    return '<h1 align = "center"><i>A Study on the Occupation of People </i>by Karen Li and Ahnaf Hasan</h1><br><a href="occupations">Click to view Occupations Table</a>'
 
-@app.route('/occupations') #occupations route
-def displayOccupations(): #renders occupation.html
-                          #end product is a table and a link back to homepage
-    return render_template("occupation.html",
-                            title = "Occupation Breakdown", #title of page
-                            header = "Occupation by Percentage", #Descriptive title
-                            Discription = "Chosen Occupation:", #Label the randomly chosen occupation
-                            extra = weightChoice(diction), #randomly chosen occupation
-                            dictionary = diction #dictionary to iterate through and find occupations
-                            ) + returnHome() #link to return to homepage
 
-def returnHome():
-    return '<a href="home">Click to go back</a>'
+weightedOccupationList = []
+
+occupationDic = {}
+
+
+
+def fillList():
+
+    #reads csv file
+
+    csvFileObject = open( 'occupations.csv', 'r')
+
+    dictionaryReader = csv.DictReader( csvFileObject)
+
+    
+
+    #looks at each row except for the last
+
+    for row in dictionaryReader:
+
+        if (row['Job Class'] != 'Total'):
+
+	    
+
+       	    #fills occupationList with occupations with frequency dependent on percentagE
+
+            occupationDic[row['Job Class']] = row['Percentage']
+
+            i = 0
+
+            while i < (float(row['Percentage'])*10):
+
+                weightedOccupationList.append(row['Job Class'])
+
+                i+=1
+
+
+
+#returns a randomly selected occupation from the weighted occupationList
+
+fillList();
+
+def randomOccupation():
+
+    return random.choice(weightedOccupationList)
+
+
+
+
+
+@app.route("/occupations")
+
+def template():
+
+    return render_template('template.html', randOcc = randomOccupation(), occList = occupationDic) 
+
+
 
 if __name__ == "__main__":
+
+    app.debug = True
+
     app.run()
