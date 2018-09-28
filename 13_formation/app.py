5f664fc9d3c1410c65e186453f7ee3d38453c25c
Karen Li <kli19@stuy.edu>
2018-09-27 22:48:22 -0400

    results page and descriptions

---
+++ 13_formation/app.py
@@ -0,0 +1,31 @@
+# Karen Li
+# SoftDev1 pd7
+# K #13: Echo Echo Echo .
+# 2018-09-27
+
+from flask import Flask, render_template, request
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/")
+def home():
+    return render_template('form.html')
+    
+
+@app.route("/auth")
+def authenticate():
+    print(app)
+    print(request)
+    print(request.args)
+    name = request.args.get("username")
+    methodUsed = request.method 
+    return render_template('results.html',
+                           field0 = "username",
+                           field1 = "method",
+                           value0 = name,
+                           value1 = methodUsed,
+                           header = "This is the information we have received:",
+                           message = "Thank you for your input.")
+
+if __name__ == "__main__":
+    app.debug = True #turn off when going live
+    app.run()
