360c35e62d69fad1b35564af4aa11b1926424801
Karen Li <kli19@stuy.edu>
2018-09-29 21:11:06 -0400

    can handle GET and POST

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -16,14 +16,20 @@ def authenticate():
     print(app)
     print(request)
     print(request.args)
-    name = request.args["username"]
-    #name = request.form["username"]
-    methodUsed = request.method 
+	
+    dictionary = {}
+
+    if request.method=="GET":
+        dictionary = request.args
+		
+    if request.method=="POST":
+        dictionary = request.form
+ 
     return render_template('results.html',
                            field0 = "username",
                            field1 = "method",
-                           value0 = name,
-                           value1 = methodUsed,
+                           value0 = dictionary["username"],
+                           value1 = request.method,
                            header = "This is the information we have received:",
                            message = "Thank you for your input.")
 
