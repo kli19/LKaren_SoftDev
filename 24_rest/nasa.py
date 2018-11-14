ca4622bde62886b28d54bba55c8030eef2465fad
Karen Li <kli19@stuy.edu>
2018-11-14 00:13:55 -0500

    comments

--- 24_rest/nasa.py
+++ 24_rest/nasa.py
@@ -5,14 +5,14 @@
 
 from flask import Flask, render_template
 import urllib, json
-app = Flask(__name__) #create instance of class Flask
+app = Flask(__name__) 
 
-@app.route("/") #assign function to route
+@app.route("/") 
 def hello_world():
      url_object = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?date=2015-01-01&api_key=vQel13DdBfcZ9IdHbivw0nuxaC6WtRuuPPqngd8H")
-    # print(url_object)
+     #print(url_object)
      info = url_object.read()
-    # print(info)
+     #print(info)
      dict = json.loads(info)
      #print(dict["url"])
      return render_template("nasa.html", image_url=dict["url"], explanation=dict["explanation"])
