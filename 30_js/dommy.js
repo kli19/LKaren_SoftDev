4196147baadcb5f98000935e125d83452a083e09
Karen Li <kli19@stuy.edu>
2018-12-20 23:30:24 -0500

    button push adds element to list

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -7,12 +7,29 @@ var fibonacci = (n) => {
   }
   return fibonacci(n-1) + fibonacci(n-2);
 }
+var list_num = 8;
+var list = document.getElementById("thelist");
+var list_button = document.getElementById("b");
 
-document.getElementById("b").addEventListener("click", function(){
-    int num = 8;
-    var list = document.getElementByID("thelist");
+var heading = document.getElementById("h");
+
+list_button.addEventListener('click', function(){
     var li = document.createElement("li");
-    li.innerHTML="item " + num;
+    li.innerHTML="item " + list_num;
     list.appendChild(li);
-    num++;
+    list_num++;
+});
+
+list.addEventListener('mouseover', function(e){
+    console.log(e.target.innerHTML);
+    heading.innerHTML = e.target.innerHTML;
+});
+
+list.addEventListener('mouseout', function(){
+    heading.innerHTML = "Hello World!";
+});
+
+list.addEventListener('click', function(e){
+    console.log(e);
+    list.removeChild(e.target);
 });
