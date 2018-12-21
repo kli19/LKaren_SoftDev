df3631d705ea7132938acaf5f9816763575091a5
Karen Li <kli19@stuy.edu>
2018-12-21 15:10:00 -0500

    beyond

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -8,6 +8,13 @@ var fibonacci = (n) => {
   return fibonacci(n-1) + fibonacci(n-2);
 }
 
+var fact = (n) => {
+  if ( n == 0 ) {
+    return 1;
+  }
+  return n * fact(n-1);
+}
+
 //list vars
 var list_num = 8;
 var list = document.getElementById("thelist");
@@ -20,6 +27,10 @@ var heading = document.getElementById("h");
 var fib_num = 0;
 var fib_list = document.getElementById("fiblist");
 var fib_button = document.getElementById("fb");
+
+//fact vars
+var fact_num = 0;
+
 
 //when the list button is clicked, another list element is appended
 list_button.addEventListener('click', function(){
@@ -54,3 +65,19 @@ fib_button.addEventListener('click', function(){
     fib_num++;
 });
 
+
+//additional name list stuff
+var fact_button = document.createElement("button");
+fact_button.innerHTML = "fact";
+fib_button.insertAdjacentElement('afterend', fact_button);
+var fact_list = document.createElement("ol");
+//fact_list.setAttribute("id", "fact_list");
+list_button.parentNode.insertBefore(fact_list, list_button);
+
+fact_button.addEventListener('click', function(){
+    var li = document.createElement("li");
+    console.log("test");
+    li.innerHTML = `fact(${fact_num}) = ` + fact(fact_num);
+    fact_list.appendChild(li);
+    fact_num++;
+});
